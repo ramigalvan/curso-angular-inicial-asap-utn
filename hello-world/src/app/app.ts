2ed4a1b5b453a805f653534f1506543e2824eb7f
ramirogalvan <rgalvan@asap-consulting.net>
2025-09-01 12:30:10 -0300

    basic angular app sandbox

---
+++ hello-world/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component  } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  // styleUrl: './app.css'
+})
+export class App {
+  // protected readonly title = signal('hello-world');
+  title = "hello world"
+}
