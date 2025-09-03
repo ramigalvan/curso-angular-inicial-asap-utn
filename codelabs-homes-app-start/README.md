78b381cf5d2fdbd155024d817ab7030119450f00
ramirogalvan <rgalvan@asap-consulting.net>
2025-09-03 12:58:26 -0300

    agregando routing

---
+++ codelabs-homes-app-start/README.md
@@ -0,0 +1,23 @@
+# Angular Homes App
+
+## youtube tutorial: [angular playlist](https://www.youtube.com/playlist?list=PL1w1q3fL4pmj9k1FrJ3Pe91EPub2_h4jF)
+
+- Install Angular if you don't have it installed
+
+  `npm install -g @angular/cli`
+
+- Clone this branch to your local machine
+
+  `git clone -b homes-app-start git@github.com:angular/codelabs.git homes-app`
+
+- Once the code has been downloaded
+
+  `cd homes-app`
+
+- Install the depencies
+
+  `npm install` 
+
+- Run the application 
+
+  `ng serve`
---
+++ codelabs-homes-app-start/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from "./home/home.component";
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  template: `
+    <main>
+      <header class="brand-name">
+        <img src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+      <section class="content">
+        <router-outlet>
+        </router-outlet>
+        <!-- <app-home></app-home> -->
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
+  imports: [HomeComponent, RouterModule],
+})
+export class AppComponent {
+  title = 'homes';
+}
